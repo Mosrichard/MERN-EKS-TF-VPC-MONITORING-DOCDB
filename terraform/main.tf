59b796b04d07a103ade7ac31f6c81d702f9e43e6
Mosrichard <mosrichard@example.com>
2026-03-06 16:20:32 +0530

    Add destroy workflow and fix terraform state handling

--- terraform/main.tf
+++ terraform/main.tf
@@ -252,7 +252,7 @@ resource "aws_iam_role_policy_attachment" "node_policies" {
 resource "aws_eks_cluster" "main" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.cluster_role.arn
-  version  = "1.31"
+  version  = "1.32"
 
   vpc_config {
     subnet_ids              = [aws_subnet.private_1.id, aws_subnet.private_2.id, aws_subnet.public_1.id, aws_subnet.public_2.id]
